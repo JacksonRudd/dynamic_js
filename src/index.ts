85b15858a2739558237b0dbb84824ff5f575be93
Jackson <jackson.h.rudd@gmail.com>
2023-02-19 16:33:42 -0800

    simple canvas

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
-// import { formData } from "./forms";
 
-// const form = document.querySelector('form')!;
 
-// form.addEventListener('submit', (e) => {
-//         e.preventDefault()
-//         const data = formData(form)
-//         console.log(data)
-//     }
-// )
-import { formData } from "./forms";
+let canvas = document.getElementById('canvas') as
+                HTMLCanvasElement;
+let context = canvas.getContext("2d")!;
+context.lineCap = 'round';
+context.lineJoin = 'round';
+context.strokeStyle = 'black';
+context.lineWidth = 1;
 
-const form = document.querySelector('form')!;
+context.arc(1,100,300,0,5)
+context.closePath()
+context.fillStyle = "rgb(200, 0, 0)";
+context.fillRect(10, 10, 50, 50);
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    const data = formData(form);
-    console.log(data);
-});
+context.fillStyle = "rgba(0, 0, 200, 0.5)";
+context.fillRect(30, 30, 50, 50);
