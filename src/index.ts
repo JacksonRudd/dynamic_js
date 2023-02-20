7856d1681889d04c0f0a02b4079f7233e71f82e0
Jackson <jackson.h.rudd@gmail.com>
2023-02-19 18:56:54 -0800

    done

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,30 @@
 
 let canvas = document.getElementById('canvas') as
                 HTMLCanvasElement;
-let context = canvas.getContext("2d")!;
-context.lineCap = 'round';
-context.lineJoin = 'round';
-context.strokeStyle = 'black';
-context.lineWidth = 1;
+canvas.width = .9*window.innerWidth
+canvas.height = .9*window.innerHeight
+let c = canvas.getContext("2d")!;
 
-context.arc(1,100,300,0,5)
-context.closePath()
-context.fillStyle = "rgb(200, 0, 0)";
-context.fillRect(10, 10, 50, 50);
 
-context.fillStyle = "rgba(0, 0, 200, 0.5)";
-context.fillRect(30, 30, 50, 50);
+let start_seconds = new Date()
+
+function get_seconds(time: Date){
+    return time.getSeconds()+ time.getMilliseconds()/1000
+}
+
+function current_t() {
+    return get_seconds(new Date()) - get_seconds(start_seconds)
+}
+
+
+function animate(){
+   
+    requestAnimationFrame(animate)
+    c.clearRect(0,0,innerWidth, innerHeight)
+    var seconds = current_t()
+    c.fillStyle = "rgb(200, 0, 0)";
+    c.fillRect((100*seconds+ 100*Math.sin(6*seconds))%canvas.width, (100*seconds) %canvas.height, 5, 5);
+
+}
+
+animate()
